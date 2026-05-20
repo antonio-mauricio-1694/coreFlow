037741cbe0a38afa4c2297017c48514c95e0b554
antonio-mauricio-1694 <antoniomauriciodev1694@gmail.com>
2026-05-20 05:35:31 -0300

    feat(backend): estrutura inicial do backend com Spring Boot, Docker e PostgreSQL

---
+++ backend/backend/src/main/java/com/coreflow/backend/domain/User.java
@@ -0,0 +1,62 @@
+package com.coreflow.backend.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
